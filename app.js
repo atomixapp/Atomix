fe497ee8fc7f02d5700bd95ea26ec2e908c48b6d
atomixapp <silver1980sm@gmail.com>
2025-07-16 20:03:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const menuUsuario = document.getElementById('menuUsuario');
   const tituloCategoria = document.getElementById('tituloCategoria');
   const sonidoClick = new Audio('assets/sounds/click.mp3');
-  const sonidoClick = new Audio('assets/sounds/click.mp3');
   const asideItems = Array.from(document.querySelectorAll('aside li'));
   
   let todasPeliculas = [];
