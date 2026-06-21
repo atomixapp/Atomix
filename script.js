883d8145e3628084c3ad0fa77db5a0b62f682e7d
atomixapp <silver1980sm@gmail.com>
2026-06-21 18:58:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ button.innerHTML = "Descargar APK";
 },3000);
 
 });
+
+window.addEventListener("scroll",()=>{
+
+    const header=document.querySelector(".topbar");
+
+    if(window.scrollY>40){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
